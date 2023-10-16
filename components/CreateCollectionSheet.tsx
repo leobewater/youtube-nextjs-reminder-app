72fa957367d4dadaa1e5dcef36ad936aaf4453fd
leobewater <mmbee888@gmail.com>
2023-10-16 14:04:48 -0400

    set up name field

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Sheet, SheetContent, SheetDescription, SheetTitle } from './ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from './ui/sheet';
 import { useForm } from 'react-hook-form';
 import {
   createCollectionSchema,
   createCollectionSchemaType,
 } from '@/schema/createCollection';
 import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
 interface Props {
   open: boolean;
   onOpenChange: (open: boolean) => void;
@@ -13,17 +28,41 @@ interface Props {
 
 const CreateCollectionSheet = ({ open, onOpenChange }: Props) => {
   const form = useForm<createCollectionSchemaType>({
-    defaultValues: {},
     resolver: zodResolver(createCollectionSchema),
+    defaultValues: {},
   });
-    
+
+  const onSubmit = (data: createCollectionSchemaType) => {
+    console.log('SUBMITTED:', data);
+  };
+
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent>
-        <SheetTitle>Add new collection</SheetTitle>
-        <SheetDescription>
-          Collections are a way to group your tasks
-        </SheetDescription>
+        <SheetHeader>
+          <SheetTitle>Add new collection</SheetTitle>
+          <SheetDescription>
+            Collections are a way to group your tasks
+          </SheetDescription>
+        </SheetHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Personal" {...field} />
+                  </FormControl>
+                  <FormDescription>Collection name</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </SheetContent>
     </Sheet>
   );
