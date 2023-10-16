953cca0a4ab313912cc3ea600ffaffdab3664a43
leobewater <mmbee888@gmail.com>
2023-10-16 13:04:01 -0400

    set up theme switcher button

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+'use client';
+
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { Tabs, TabsList, TabsTrigger } from './ui/tabs';
+import { DesktopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
 
 const ThemeSwitcher = () => {
-  return (
-    <div>ThemeSwitcher</div>
-  )
-}
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-export default ThemeSwitcher
+  // useEffect ony runs on the client, so now we can safely show the UI
+  // this trick will avoid hydration error
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className="border dark:border-neutral-800 dark:bg-[#030303]">
+        <TabsTrigger value="light" onClick={() => setTheme('light')}>
+          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+        <TabsTrigger value="dark" onClick={() => setTheme('dark')}>
+          <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0" />
+        </TabsTrigger>
+        <TabsTrigger value="system" onClick={() => setTheme('system')}>
+          <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+};
+
+export default ThemeSwitcher;
