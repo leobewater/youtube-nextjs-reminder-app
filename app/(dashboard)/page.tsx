fe75cc92d516ab5794f5151d4e6f210a92fe42d8
leobewater <mmbee888@gmail.com>
2023-10-16 14:31:03 -0400

    add collection card component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import CollectionCard from '@/components/CollectionCard';
 import CreateCollectionBtn from '@/components/CreateCollectionBtn';
 import SadFace from '@/components/icons/SadFace';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
@@ -77,8 +78,10 @@ const CollectionList = async () => {
 
   return (
     <div>
-      Collections: {collections.length}
       <CreateCollectionBtn />
+      {collections.map((collection) => (
+        <CollectionCard key={collection.id} collection={collection} />
+      ))}
     </div>
   );
 };
---
+++ components/CollectionCard.tsx
@@ -0,0 +1,12 @@
+import { Collection } from '@prisma/client';
+import React from 'react';
+
+interface Props {
+  collection: Collection;
+}
+
+const CollectionCard = ({ collection }: Props) => {
+  return <div>{collection.name}</div>;
+};
+
+export default CollectionCard;
